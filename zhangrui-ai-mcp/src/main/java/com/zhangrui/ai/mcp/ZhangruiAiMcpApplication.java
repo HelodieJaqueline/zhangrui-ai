4f60d4f194876e4cb669d9581a17d63e69b5f54a
zhangrui <zhangrui1@able-elec.com>
2026-01-19 16:10:59 +0800

    init

---
+++ zhangrui-ai-mcp/src/main/java/com/zhangrui/ai/mcp/ZhangruiAiMcpApplication.java
@@ -0,0 +1,13 @@
+package com.zhangrui.ai.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhangruiAiMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhangruiAiMcpApplication.class, args);
+    }
+
+}
---
+++ zhangrui-ai-mcp/src/test/java/com/zhangrui/ai/mcp/ZhangruiAiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangrui.ai.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhangruiAiMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
